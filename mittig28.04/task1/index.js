7496ca86b89478b52f77579e0f9999e3b529bfa2
Ksondzyk <roman.kson@gmail.com>
2020-04-28 11:34:44 +0300

    push

---
+++ mittig28.04/task1/index.js
@@ -0,0 +1,18 @@
+// ... code here
+const numbersList = [4.78, 6.9, 3.666];
+
+// function callback(acc, num) {
+//   console.log(acc, num);
+//   const newAcc = acc + num;
+//   // return undefined;
+// }
+
+// const sum = numbersList.reduce(callback, 0);
+// console.log(sum);
+
+const getTotaPrice = (arr) => {
+  const sum = arr.reduce(function callback(acc, num) {
+    return acc + num;
+  }, 0);
+  return "$" + Math.floor(sum * 100) / 100;
+};
