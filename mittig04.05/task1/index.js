5f5f0819ec2c4d9261086e21cad5344411071959
Ksondzyk <roman.kson@gmail.com>
2020-05-04 11:49:03 +0300

    push

--- mittig04.05/task1/index.js
+++ mittig04.05/task1/index.js
@@ -1,21 +1,52 @@
-function addPropertyV1(userData, userId) {
-  userData.id = userId;
-  return userData;
-}
-
-// function addPropertyV2(userData, userId) {
-//   Object.assign(userData, { id: userId });
+// function addPropertyV1(userData, userId) {
+//   userData.id = userId;
 //   return userData;
 // }
 
-const addPropertyV2 = (userData, userId) =>
-  Object.assign(userData, { id: userId });
+// // function addPropertyV2(userData, userId) {
+// //   Object.assign(userData, { id: userId });
+// //   return userData;
+// // }
 
-const user = { name: "test", age: 20 };
+// const addPropertyV2 = (userData, userId) =>
+//   Object.assign(userData, { id: userId });
 
-const addPropertyV3 = (userData, userId) =>
-  Object.assign({}, userData, { id: userId });
+// const user = { name: "test", age: 20 };
 
-function addPropertyV4userData, userId) {
-  return { ...userData, id: userId };
-}
+// const addPropertyV3 = (userData, userId) =>
+//   Object.assign({}, userData, { id: userId });
+
+// function addPropertyV4userData, userId) {
+//   return { ...userData, id: userId };
+// }
+
+const user = {
+  name: "Tom",
+};
+
+const addPropertyV1 = (userData, userId) => {
+  userData.id = userId;
+  return userData;
+};
+const result = addPropertyV1(user, "12345678");
+console.log(result);
+
+const addPropertyV2 = (userData, userId) => {
+  return Object.assign(userData, { id: userId });
+};
+const result2 = addPropertyV2(user, "1234567");
+console.log(result2);
+
+const addPropertyV3 = (userData, userId) => {
+  const user2 = { ...userData, id: userId };
+  return user2;
+};
+const result3 = addPropertyV3(user, "1234567");
+console.log(result3);
+
+const addPropertyV4 = (userData, userId) => {
+  const user3 = { ...userData, id: userId };
+  return user3;
+};
+const result4 = addPropertyV4(user, "1234567");
+console.log(result4);
