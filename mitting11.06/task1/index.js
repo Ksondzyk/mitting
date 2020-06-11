299aac3bf4f8fbc74187695628d12c13995beb3f
roman.kson@gmail.com <roman.kson@gmail.com>
2020-06-11 11:30:52 +0300

    push

---
+++ mitting11.06/task1/index.js
@@ -0,0 +1,51 @@
+const baseUrl = "https://5ece5f5561c8480016701459.mockapi.io/api/v1/users";
+
+export const getUsersList = () => {
+  return fetch(baseUrl).then((response) => response.json());
+};
+// getTasksList();
+
+export const getUserById = (userId) => {
+  return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
+};
+// getUserById("12");
+
+export const createUser = (createUser) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(createUser),
+  }).then((response) => response.json());
+};
+// createUser({
+//   id: "1",
+//   email: "cool@email.com",
+//   firstName: "Iron",
+//   lastName: "Man",
+//   age: 42,
+// });
+
+export const updateUser = (userId, userObject) => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userObject),
+  }).then((response) => response.json());
+};
+// updateUser("1", {
+//   id: "1",
+//   email: "roman@email.com",
+//   firstName: "Roman",
+//   lastName: "Kson",
+//   age: 26,
+// });
+
+export const deleteUser = (userId) => {
+  return fetch(`${baseUrl}/${userId}`, { method: "DELETE" }).then((response) =>
+    response.json()
+  );
+};
